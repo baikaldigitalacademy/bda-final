36b915b3c0e478b7b242898e1509807788e16128
FroZo <frozo02@yandex.ru>
2021-10-08 21:49:19 +0800

    [edit] minor changes

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,7 +4,7 @@
 ] )
 
 @push( "styles" )
-    <link rel = "stylesheet" type = "text/css" href = "{{ asset( "css/index.css" ) }}">
+    <link rel = "stylesheet" type = "text/css" href = "{{ asset( "css/edit.css" ) }}">
     <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
 @endpush
 
@@ -26,105 +26,104 @@
             @method("put")
         @endif
 
-        <a href="." class="button">Cancel</a>
-        <input type="submit" value="Save">
+        <input type="submit" value="Сохранить">
+        <a href="." class="button">Назад</a>
         <fieldset>
-            <legend>CV information</legend>
-            <fieldset>
-                <legend>Полное имя</legend>
-                <input size="100%"
-                    name="name"
-                    id = "name"
-                    value = "{{old('name') ?? $data->name}}"
+            <legend>Полное имя</legend>
+            <input size="100%"
+                name="name"
+                id = "name"
+                value = "{{old('name') ?? $data->name}}"
+            >
+        </fieldset>
+        <fieldset>
+            <legend>Дата собеседования</legend>
+            <input
+                type="date"
+                name="date"
+                value="{{old('date') ?? $data->date}}"
+            >
+        </fieldset>
+        <fieldset>
+            <legend>E-mail</legend>
+            <input size="100%"
+                type="email"
+                id = "email"
+                name="email"
+                value="{{old('email') ?? $data->email}}"
+            >
+        </fieldset>
+        <fieldset>
+            <legend>Статус</legend>
+            <select name="status_id">
+                @foreach($statuses as $it)
+                    <option value = {{$it->id}}
+                    @if($it->id == old("status_id") or $it->name == $data->status)
+                         selected>
+                    @else
+                        >
+                    @endif
+                        {{$it->name}}
+                    </option>
+                @endforeach
+            </select>
+        </fieldset>
+        <fieldset>
+            <legend>Уровень</legend>
+            <select name="level_id">
+                <option
+                    value = ""
+                    {{ !$data->level ? "selected" : "" }}
                 >
-            </fieldset>
-            <fieldset>
-                <legend>Дата собеседования</legend>
-                <input
-                    type="date"
-                    name="date"
-                    value="{{old('date') ?? $data->date}}"
-                >
-            </fieldset>
-            <fieldset>
-                <legend>E-mail</legend>
-                <input size="100%"
-                    type="email"
-                    id = "email"
-                    name="email"
-                    value="{{old('email') ?? $data->email}}"
-                >
-            </fieldset>
-            <fieldset>
-                <legend>Статус</legend>
-                <select name="status_id">
-                    @foreach($statuses as $it)
-                        <option value = {{$it->id}}
-                        @if($it->id == old("status_id") or $it->name == $data->status)
-                             selected>
+                    Без уровня
+                </option>
+                @foreach($levels as $it)
+                    <option value = {{$it->id}}
+                    @if($it->id == old("level_id") or $it->name == $data->level)
+                        selected>
                         @else
                             >
                         @endif
-                            {{$it->name}}
-                        </option>
-                    @endforeach
-                </select>
-            </fieldset>
-            <fieldset>
-                <legend>Уровень</legend>
-                <select name="level_id">
-                    <option
-                        value = ""
-                        {{ !$data->level ? "selected" : "" }}
-                    >
-                        Без уровня
+                        {{$it->name}}
                     </option>
-                    @foreach($levels as $it)
-                        <option value = {{$it->id}}
-                        @if($it->id == old("level_id") or $it->name == $data->level)
-                            selected>
-                            @else
-                                >
-                            @endif
-                            {{$it->name}}
-                        </option>
-                    @endforeach
-                </select>
-            </fieldset>
-            <fieldset>
-                <legend>Позиция</legend>
-                <select name="position_id">
-                    @foreach($positions as $it)
-                        <option value = {{$it->id}}
-                        @if($it->id == old("position_id") or $it->name == $data->position)
-                            selected>
-                            @else
-                                >
-                            @endif
-                            {{$it->name}}
-                        </option>
-                    @endforeach
-                </select>
-            </fieldset>
-            <fieldset>
-                <legend>Навыки</legend>
-                <div id = "skillsEditorDiv">
-                    {!! old('skills') ?? $data->skills !!}
-                </div>
-            </fieldset>
-            <fieldset>
-                <legend>Описание</legend>
-                <div id = "descriptionEditorDiv">
-                    {!! old('description') ?? $data->description !!}
-                </div>
-            </fieldset>
-            <fieldset>
-                <legend>Опыт</legend>
-                <div id = "experienceEditorDiv">
-                    {!! old('experience') ?? $data->experience !!}
-                </div>
-            </fieldset>
+                @endforeach
+            </select>
         </fieldset>
+        <fieldset>
+            <legend>Позиция</legend>
+            <select name="position_id">
+                @foreach($positions as $it)
+                    <option value = {{$it->id}}
+                    @if($it->id == old("position_id") or $it->name == $data->position)
+                        selected>
+                        @else
+                            >
+                        @endif
+                        {{$it->name}}
+                    </option>
+                @endforeach
+            </select>
+        </fieldset>
+        <fieldset>
+            <legend>Навыки</legend>
+            <div id = "skillsEditorDiv" class = "editor">
+                {!! old('skills') ?? $data->skills !!}
+            </div>
+        </fieldset>
+        <fieldset>
+            <legend>Описание</legend>
+            <div id = "descriptionEditorDiv" class = "editor">
+                {!! old('description') ?? $data->description !!}
+            </div>
+        </fieldset>
+        <fieldset>
+            <legend>Опыт</legend>
+            <div id = "experienceEditorDiv" class = "editor">
+                {!! old('experience') ?? $data->experience !!}
+            </div>
+        </fieldset>
+        <input type="submit" value="Сохранить">
+        <a href="." class="button">Назад</a>
         <input id = "skills" name = "skills" type = "text" hidden>
         <input id = "description" name = "description" type = "text" hidden>
         <input id = "experience" name = "experience" type = "text" hidden>
