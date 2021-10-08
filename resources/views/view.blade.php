dd636f532817a69f48a3870343180d595adfb687
FroZo <frozo02@yandex.ru>
2021-10-08 23:22:41 +0800

    [view] remove fieldset

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -17,42 +17,39 @@
     <button onclick = "destroy( '{{ route( "summaries_destroy", [ "summary" => $data->id ] ) }}' )">Удалить</button>
     <a href = "{{ route( "pdf", [ "id" => $data->id ] ) }}" class = "button">Скачать PDF</a>
     <fieldset>
-        <legend>CV information</legend>
-        <fieldset>
-            <legend>Полное имя</legend>
-            {{$data->name}}
-        </fieldset>
-        <fieldset>
-            <legend>Дата собеседования</legend>
-            {{$data->date}}
-        </fieldset>
-        <fieldset>
-            <legend>E-mail</legend>
-            {{$data->email}}
-        </fieldset>
-        <fieldset>
-            <legend>Статус</legend>
-            {{$data->status}}
-        </fieldset>
-        <fieldset>
-            <legend>Уровень</legend>
-            {{$data->level}}
-        </fieldset>
-        <fieldset>
-            <legend>Позиция</legend>
-            {{$data->position}}
-        </fieldset>
-        <fieldset>
-            <legend>Навыки</legend>
-                {!! $data->skills !!}
-        </fieldset>
-        <fieldset>
-            <legend>Описание</legend>
-                {!! $data->description !!}
-        </fieldset>
-        <fieldset>
-            <legend>Опыт</legend>
-                {!! $data->experience !!}
-        </fieldset>
+        <legend>Полное имя</legend>
+        {{$data->name}}
+    </fieldset>
+    <fieldset>
+        <legend>Дата собеседования</legend>
+        {{$data->date}}
+    </fieldset>
+    <fieldset>
+        <legend>E-mail</legend>
+        {{$data->email}}
+    </fieldset>
+    <fieldset>
+        <legend>Статус</legend>
+        {{$data->status}}
+    </fieldset>
+    <fieldset>
+        <legend>Уровень</legend>
+        {{$data->level}}
+    </fieldset>
+    <fieldset>
+        <legend>Позиция</legend>
+        {{$data->position}}
+    </fieldset>
+    <fieldset>
+        <legend>Навыки</legend>
+            {!! $data->skills !!}
+    </fieldset>
+    <fieldset>
+        <legend>Описание</legend>
+            {!! $data->description !!}
+    </fieldset>
+    <fieldset>
+        <legend>Опыт</legend>
+            {!! $data->experience !!}
     </fieldset>
 @endsection
