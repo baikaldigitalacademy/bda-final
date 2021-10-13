45001e3c69cff80096408a51ae8931b04474aafa
FroZo <frozo02@yandex.ru>
2021-10-14 01:40:10 +0800

    [redesign] fix colors

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -33,24 +33,24 @@
         </div>
         <div class = "row text-black mt-3">
             <div class = "col-lg-4">
-                <div class="card bg-light mb-3">
-                    <div class="card-header">Полное имя</div>
+                <div class="card bg-my-secondary text-white mb-3">
+                    <div class="card-header fw-bold">Полное имя</div>
                     <div class="card-body">
                         <p class="card-text">{{ $data->name }}</p>
                     </div>
                 </div>
             </div>
             <div class = "col-lg-4">
-                <div class="card bg-light mb-3">
-                    <div class="card-header">Дата собеседования</div>
+                <div class="card bg-my-secondary text-white mb-3">
+                    <div class="card-header fw-bold">Дата собеседования</div>
                     <div class="card-body">
                         <p class="card-text">{{ $data->date }}</p>
                     </div>
                 </div>
             </div>
             <div class = "col-lg-4">
-                <div class="card bg-light mb-3">
-                    <div class="card-header">E-Mail</div>
+                <div class="card bg-my-secondary text-white mb-3">
+                    <div class="card-header fw-bold">E-Mail</div>
                     <div class="card-body">
                         <p class="card-text">{{ $data->email }}</p>
                     </div>
@@ -59,32 +59,32 @@
         </div>
         <div class = "row text-black">
             <div class = "col-lg-3">
-                <div class="card bg-light mb-3">
-                    <div class="card-header">Статус</div>
+                <div class="card bg-my-secondary text-white mb-3">
+                    <div class="card-header fw-bold">Статус</div>
                     <div class="card-body">
                         <p class="card-text">{{ $data->status }}</p>
                     </div>
                 </div>
             </div>
             <div class = "col-lg-3">
-                <div class="card bg-light mb-3">
-                    <div class="card-header">Уровень</div>
+                <div class="card bg-my-secondary text-white mb-3">
+                    <div class="card-header fw-bold">Уровень</div>
                     <div class="card-body">
                         <p class="card-text">{{ $data->level ?? "N/A" }}</p>
                     </div>
                 </div>
             </div>
             <div class = "col-lg-3">
-                <div class="card bg-light mb-3">
-                    <div class="card-header">Позиция</div>
+                <div class="card bg-my-secondary text-white mb-3">
+                    <div class="card-header fw-bold">Позиция</div>
                     <div class="card-body">
                         <p class="card-text">{{ $data->position }}</p>
                     </div>
                 </div>
             </div>
             <div class = "col-lg-3">
-                <div class="card bg-light mb-3">
-                    <div class="card-header">Кто завёл резюме</div>
+                <div class="card bg-my-secondary text-white mb-3">
+                    <div class="card-header fw-bold">Кто завёл резюме</div>
                     <div class="card-body">
                         <p class="card-text">{{ $data->user_name }}</p>
                     </div>
@@ -93,8 +93,8 @@
         </div>
         <div class = "row text-black">
             <div class = "col-12">
-                <div class = "card bg-light">
-                    <div class="card-header">Навыки</div>
+                <div class = "card bg-my-secondary text-white">
+                    <div class="card-header fw-bold">Навыки</div>
                     <div class="card-body">
                         {!! $data->skills !!}
                     </div>
@@ -103,8 +103,8 @@
         </div>
         <div class = "row text-black mt-3">
             <div class = "col-12">
-                <div class = "card bg-light">
-                    <div class="card-header">Описание</div>
+                <div class = "card bg-my-secondary text-white">
+                    <div class="card-header fw-bold">Описание</div>
                     <div class="card-body">
                         {!! $data->description !!}
                     </div>
@@ -113,8 +113,8 @@
         </div>
         <div class = "row text-black mt-3">
             <div class = "col-12">
-                <div class = "card bg-light">
-                    <div class="card-header">Опыт</div>
+                <div class = "card bg-my-secondary text-white">
+                    <div class="card-header fw-bold">Опыт</div>
                     <div class="card-body">
                         {!! $data->experience !!}
                     </div>
